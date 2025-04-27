a360838895983588c7b387c5d61c546fed3bff57
Muhammed Kızıldağ <mhmmd0536@gmail.com>
2025-04-27 19:58:58 +0300

    update

--- lib/providers/WeatherProvider.dart
+++ lib/providers/WeatherProvider.dart
@@ -7,10 +7,22 @@ import 'package:weatherapp/providers/LocationProvider.dart';
 final weatherProvider = FutureProvider(
   (ref) async {
     Location local = ref.watch(locationState);
-    var response = await http.get(
-      Uri.parse('http://10.0.2.2:3000/?city=${local.city}&county=${local.county}'),
-      // headers: {'city': local.city, 'county': local.county}
-    );
-    return jsonDecode(response.body);
+    var r = await http.get(Uri.parse('https://servis.mgm.gov.tr/web/merkezler?il=${local.city}&ilce=${local.county}'),
+        headers: {'Origin': 'https://www.mgm.gov.tr'});
+
+    var j = jsonDecode(r.body);
+
+    var r2 = await http.get(Uri.parse('https://servis.mgm.gov.tr/web/sondurumlar?merkezid=${j[0]["merkezId"]}'),
+        headers: {'Origin': 'https://www.mgm.gov.tr'});
+
+    print(r2.body);
+
+    var j2 = jsonDecode(r2.body);
+
+    // var response = await http.get(
+    //   Uri.parse('http://10.0.2.2:3000/?city=${local.city}&county=${local.county}'),
+    // );
+
+    return {'temp': j2[0]['sicaklik'], 'link': 'https://www.mgm.gov.tr/Images_Sys/hadiseler/${j2[0]['hadiseKodu']}.svg'};
   },
 );
